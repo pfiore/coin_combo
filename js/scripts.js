9053499e2cf9dcec7e13676d37038ed4bb151627
Patrick Fiorentino & Tom Mertz <student@epicodus.com>
2015-04-08 10:36:01 -0700

    create and pass specs thru dimes value

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 var coinCombo = function(cents) {
+
+  //an array where coins[0] = pennies, coins[1] = nickles, [2] = dimes, [3] = quarters
+  var coins = [0, 0, 0, 0];
+
   // var cents = 1;
-  return cents + " Penny";
+  var dimes = parseInt(cents / 10);
+  coins[2] = dimes;
+  var cents_remaining = cents % 10;
+
+  var nickles = parseInt(cents_remaining / 5);
+  coins[1] = nickles;
+  var cents_remaining = cents % 5;
+
+  var pennies = cents_remaining;
+  coins[0] = pennies;
+
+
+  return coins;
+
 };
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,21 @@
 describe('coinCombo', function() {
     it('takes 1 and returns, 1 Penny.', function() {
-        expect(coinCombo(1)).to.equal("1 Penny");
+        expect(coinCombo(1)).to.deep.equal([1, 0, 0, 0]);
     });
 
     it('takes 2 and returns, 2 Penny.', function() {
-        expect(coinCombo(2)).to.equal("2 Penny");
+        expect(coinCombo(2)).to.deep.equal([2, 0, 0, 0]);
+    });
+
+    it('takes 5 and returns, 1 Nickle.', function() {
+        expect(coinCombo(5)).to.deep.equal([0, 1, 0, 0]);
+    });
+
+    it('takes 6 and returns, 1 Penny, 1 Nickle.', function() {
+        expect(coinCombo(6)).to.deep.equal([1, 1, 0, 0]);
+    });
+
+    it('takes 10 and returns, 1 Dime', function() {
+        expect(coinCombo(10)).to.deep.equal([0, 0, 1, 0]);
     });
 });
